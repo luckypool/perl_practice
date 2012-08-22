ab383c161c08b262f2843d6145ae01881cea8b48
Yuta YOSHIIKE <luckypool314@gmail.com>
2012-08-23 00:09:26 +0900

    modified MockPractice : add new function

--- lib/MockPractice.pm
+++ lib/MockPractice.pm
@@ -2,4 +2,18 @@ package MockPractice;
 use strict;
 use warnings;
 
+use MockPractice::CheckHoge;
+use MockPractice::CheckFuga;
+
+use base qw/Class::Accessor::Fast/;
+
+sub new {
+    my $class = shift;
+    my hoge = MockPractice::CheckHoge->new;
+    my $self = {
+        hoge => $hoge,
+        fuga => MockPractice::CheckFuga->new;
+    };
+}
+
 1;
