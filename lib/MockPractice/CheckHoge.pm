70e0046621cdef80d84639f337ce2cdfd02ef501
Yuta YOSHIIKE <luckypool314@gmail.com>
2012-08-23 09:05:34 +0900

    modified MockPractice::CheckHoge

--- lib/MockPractice/CheckHoge.pm
+++ lib/MockPractice/CheckHoge.pm
@@ -2,9 +2,18 @@ package MockPractice::CheckHoge;
 use strict;
 use warnings;
 
+use MockPractice::CheckHoge::Piyo;
+
+use base qw/Class::Accessor::Fast/;
+__PACKAGE__->mk_accessors(qw/piyo/);
+
 sub new {
     my $class = shift;
-    return bless {}, $class;
+    my $piyo = MockPractice::CheckHoge::Piyo->new;
+    my $self = {
+        piyo => $piyo,
+    };
+    return bless $self, $class;
 }
 
 sub is_hoge {
