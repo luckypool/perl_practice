c9c472d92f589b6476e9234c4174dc918b1d5385
Yuta YOSHIIKE <luckypool314@gmail.com>
2012-08-23 09:04:48 +0900

    add MockPractice::CheckHoge::Piyo

---
+++ lib/MockPractice/CheckHoge/Piyo.pm
@@ -0,0 +1,17 @@
+package MockPractice::CheckFuga::Piyo;
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    return bless {}, $class;
+}
+
+sub is_piyo {
+    my $self = shift;
+    my ($param) = @_;
+    return 1 if $param;
+    return;
+}
+
+1;
